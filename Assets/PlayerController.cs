094c8ce7f4b357c17e37be5a8e6de350ffabc753
Mihai Zăvoian <mihaitszavo@gmail.com>
2021-04-06 03:01:46 +0300

    fix mouse look precision, refactor PlayerController

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,48 +2,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float LookSpeed = 20;
-    public float MoveSpeed = .01f;
+    public float LookSpeed = 5;
+    public float MoveSpeed = 5;
 
-    private float _forward;
-    private float _right;
+    private float _lookUp;
+    private float _lookRight;
+
+    private float _moveForward;
+    private float _moveRight;
 
     public void Start()
     {
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    public void FixedUpdate()
+    public void Update()
     {
-        transform.rotation = Quaternion.Euler(
-            ClampYRotation(transform.eulerAngles.x - Input.GetAxis("Mouse Y") * LookSpeed),
-            transform.eulerAngles.y + Input.GetAxis("Mouse X") * LookSpeed,
-            transform.eulerAngles.z);
+        UpdateInput();
 
-        UpdateMoveInput();
+
+        var xAngle = ClampYAngle(transform.eulerAngles.x - _lookUp    * LookSpeed);
+        var yAngle =             transform.eulerAngles.y + _lookRight * LookSpeed;
+
+        transform.rotation = Quaternion.Euler(xAngle, yAngle, transform.eulerAngles.z);
+
 
         var horizontalForward = transform.forward;
         horizontalForward.y = 0;
         horizontalForward.Normalize();
 
-        transform.position = transform.position
-                             + horizontalForward * _forward * MoveSpeed
-                             + transform.right   * _right   * MoveSpeed; 
+        transform.position = transform.position 
+            + horizontalForward * _moveForward * MoveSpeed * Time.deltaTime
+            + transform.right   * _moveRight   * MoveSpeed * Time.deltaTime;
     }
 
-    private void UpdateMoveInput()
+    private void UpdateInput()
     {
-        _forward = 0;
-        _right   = 0;
+        _moveForward = 0;
+        _moveRight   = 0;
 
-        _forward += Input.GetKey("w") ? 1 : 0;
-        _forward -= Input.GetKey("s") ? 1 : 0;
+        _moveForward += Input.GetKey("w") ? 1 : 0;
+        _moveForward -= Input.GetKey("s") ? 1 : 0;
 
-        _right += Input.GetKey("d") ? 1 : 0;
-        _right -= Input.GetKey("a") ? 1 : 0;
+        _moveRight   += Input.GetKey("d") ? 1 : 0;
+        _moveRight   -= Input.GetKey("a") ? 1 : 0;
+
+        _lookUp    = Input.GetAxis("Mouse Y");
+        _lookRight = Input.GetAxis("Mouse X");
     }
 
-    private float ClampYRotation(float angle)
+    private static float ClampYAngle(float angle)
     {
         angle = angle > 180 ? angle - 360 : angle; 
 
